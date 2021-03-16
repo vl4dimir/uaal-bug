250d86e432bcc5956238396fd0d376b13e253d7c
Vladimir Mitrović <v@brutalism.rs>
2021-03-16 09:06:51 +0100

    Add Unity-as-a-library to native project, `Failed to load libmain.so` error occurs when running on device

--- uaal-bug-native/app/src/main/java/com/example/uaal_bug_native/MainActivity.kt
+++ uaal-bug-native/app/src/main/java/com/example/uaal_bug_native/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.uaal_bug_native
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.unity3d.player.UnityPlayerActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +17,9 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val intent = Intent(this, UnityPlayerActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+            startActivityForResult(intent, 1)
         }
     }
 
